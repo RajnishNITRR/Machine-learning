d4c841409caebc219305aead63f7412f92b576a4
Rajnish Kumar <rajnish1800084@gmail.com>
2024-07-28 02:19:24 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='machine learning project',
+    version='0.0.0.1',
+    author='Rajnish',
+    author_email='rajnish1800084@gmail.com',
+    packages=find_packages(),
+    install_requirement=['pandas','numpy','seaborn']
+
+)
